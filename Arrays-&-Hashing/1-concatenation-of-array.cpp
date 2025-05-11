095f3c760b17754504681c1e2e5b3ac30c81b502
Abdullah Ahmed <abdullahahmed2004@gmail.com>
2025-05-11 11:49:21 +0500

    Question 1 Completed

---
+++ Arrays-&-Hashing/1-concatenation-of-array.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using std::vector;
+
+class Solution {
+  public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        size_t oldSize = nums.size();
+        nums.resize(oldSize * 2);
+        for (size_t i = 0; i < oldSize; i++) {
+            nums[i + oldSize] = nums[i];
+        }
+
+        return nums;
+    }
+};
